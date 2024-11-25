e783270f23d8926557592a34205ad11383916aa3
Weroni-K <104148980+Weroni-K@users.noreply.github.com>
2024-11-25 22:28:38 +0200

    adjusted HeaderSection

--- src/components/HeaderSection.vue
+++ src/components/HeaderSection.vue
@@ -17,9 +17,9 @@ import { defineProps, defineEmits } from 'vue'
 import MoonIcon from '../assets/icons/icon-moon.svg'
 import SunIcon from '../assets/icons/icon-sun.svg'
 
-// const props = defineProps({
-//   isDarkMode: Boolean,
-// })
+const props = defineProps({
+  isDarkMode: Boolean,
+})
 
 const emit = defineEmits(['toggle-dark-mode'])
 
@@ -30,12 +30,13 @@ const toggleDarkMode = () => {
 
 <style scoped>
 .navbar {
-  padding-top: 3rem;
-  justify-content: center;
-  padding-bottom: 2rem;
+  padding: 0;
+  margin: auto;
+  margin-bottom: 3rem;
   width: 100%;
   display: flex;
   flex-direction: row;
+  justify-content: space-between;
   align-items: center;
 }
 
@@ -43,12 +44,11 @@ const toggleDarkMode = () => {
   display: flex;
   justify-content: flex-end;
   align-items: center;
-  margin-right: 16px;
 }
 
 .theme-icon {
-  width: 24px;
-  height: 24px;
+  height: 28px;
+  aspect-ratio: 1;
   cursor: pointer;
   transition:
     transform 0.3s ease,
